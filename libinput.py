a3f9c87a1513d4dadd1f407004e76c0dcff47412
elliottwise <pikachuman20@outlook.com>
2025-10-21 12:36:55 -0400

    Modified libinput.py and main.py

--- libinput.py
+++ libinput.py
@@ -4,12 +4,16 @@ from io import BytesIO
 from PIL import Image
 
 def get_key():
+    global homework_list
+    global key_list
+
     while True:
-        key_path = input("Enter the full extended file path of the answer key: ")
-        if os.path.exists(key_path) and ".png" in key_path or ".jpeg" in key_path or ".jpg" in key_path:
-            return convert_to_base64(key_path)
-        else:
-            print("This path is invalid! Check that the file exists and is either a PNG or JPG.")
+        while True:
+            key_path = input("Enter the full extended file path of the answer key: ")
+            if os.path.exists(key_path) and ".png" in key_path or ".jpeg" in key_path or ".jpg" in key_path:
+                return (convert_to_base64(key_path))
+            else:
+                print("This path is invalid! Check that the file exists and is either a PNG or JPG.")
 
 def get_homework():
     while True:
--- main.py
+++ main.py
@@ -1,5 +1,42 @@
-# New main.py
-
-import langchain
+import ollama
 import libinput
+import os
+import time
+from langchain_community.chat_models import ChatOllama
+
+model = "gemma3:latest"
+
+homework_list = []
+key_list = []
+
+print(ollama.list())
+if model in str(ollama.list()):
+    print("The selected model is available!")
+else:
+    print("The selected model is not available!")
+    print("Downloading now")
+    ollama.pull(model)
+
+vision_llm = ChatOllama(base_url="http://localhost:11434", model=model)
+
+while True:
+    homework = libinput.get_homework()
+    will_continue = input("Would you like to enter more pages? (Y/N): ")
+    if will_continue == "Y" or will_continue == "y" or will_continue == "Yes" or will_continue == "yes":
+        homework_list.append(homework)
+        continue
+    if will_continue == "N" or will_continue == "n" or will_continue == "no" or will_continue == "No":
+        homework_list.append(homework)
+        break
+
+
+while True:
+    key = libinput.get_key()
+    will_continue = input("Would you like to enter more pages? (Y/N): ")
+    if will_continue == "Y" or will_continue == "y" or will_continue == "Yes" or will_continue == "yes":
+        key_list.append(key)
+        continue
+    if will_continue == "N" or will_continue == "n" or will_continue == "no" or will_continue == "No":
+        key_list.append(key)
+        break
 
